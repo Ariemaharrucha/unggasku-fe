73b96b1deaa6eaae6600ed705762a3ec89f228c5
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-03 14:08:07 +0700

    add card artikel & halaman artikel

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "../pages/authentication/Login.jsx";
 import { Register } from "../pages/authentication/Register.jsx";
+import { Artikel } from "../pages/clients/Artikel.jsx";
 
 export const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ export const AppRouter = () => {
     <Routes>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/register" element={<Register/>}></Route>
+      <Route path="/artikel" element={<Artikel/>}></Route>
     </Routes>
   </BrowserRouter>
   )
