8b60a57e8bec678d0bf5f724a922ae043427ce38
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-05 21:33:23 +0700

    add form create artikel

---
+++ src/pages/dashboard/FormAddArtikel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DashboardLayout } from '../../layouts/DashboardLayout.jsx'
+
+export const FormAddArtikel = () => {
+  return (
+    <DashboardLayout>
+        <section className='max-w-2xl'>
+            
+        </section>
+    </DashboardLayout>
+  )
+}
