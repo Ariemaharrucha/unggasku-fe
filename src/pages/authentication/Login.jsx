87c1ada56ce63a5544837be463dd3fd3561de19b
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-07 19:31:06 +0700

    add toggle btn password

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -2,10 +2,23 @@ import { AuthLayout } from "../../layouts/AuthLayout.jsx";
 import Button from "../../components/ui/Button.jsx";
 import Input from "../../components/ui/Input.jsx";
 import { Link } from "react-router-dom";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
 
 export const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  function handleChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function togglePassword() {
+    setShowPassword(!showPassword);
+  }
+
   return (
-    <AuthLayout title='Login Sekarang'>
+    <AuthLayout title="Login Sekarang">
       <form className="space-y-4">
         <div>
           <h5 className="text-white font-semibold">Email</h5>
@@ -17,11 +30,25 @@ export const Login = () => {
         </div>
         <div>
           <h5 className="text-white font-semibold">Password</h5>
-          <Input
-            className={"bg-transparent placeholder:text-white text-white mt-1"}
-            type="password"
-            placeholder="password"
-          ></Input>
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="block w-full border font-medium placeholder:font-light focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-primary-500 shadow-sm transition duration-200 py-2 px-3 text-base rounded-lg bg-transparent placeholder:text-white text-white mt-1"
+              placeholder="password"
+              value={password}
+              onChange={handleChange}
+            />
+            <div
+              className="absolute inset-y-0 end-0 flex items-center cursor-pointer pe-3"
+              onClick={togglePassword}
+            >
+              {showPassword ? (
+                <AiOutlineEye size={24} color="white" />
+              ) : (
+                <AiOutlineEyeInvisible size={24} color="white" />
+              )}
+            </div>
+          </div>
         </div>
         <Button
           variant="secondary"
