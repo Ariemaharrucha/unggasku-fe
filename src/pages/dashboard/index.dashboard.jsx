1a56b755d287d03f74b6db1220b6a117272f30be
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-08 16:34:09 +0700

    add new user table

--- src/pages/dashboard/index.dashboard.jsx
+++ src/pages/dashboard/index.dashboard.jsx
@@ -4,13 +4,12 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { LuFiles } from "react-icons/lu";
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
-
-const artikel = [];
+import { NewUserTable } from "./NewUserTable.jsx";
 
 export const Dashboard = () => {
   return (
     <DashboardLayout>
-      <section className="max-w-6xl">
+      <main className="max-w-6xl fixed w-full">
         <section className="sticky top-0 z-[2] flex items-center justify-between bg-white py-2">
           <div className="flex items-center gap-3">
             <div className="bg-secondary-300 flex justify-center items-center p-2 rounded-lg">
@@ -35,52 +34,12 @@ export const Dashboard = () => {
             </div>
             <h3 className="text-xl font-semibold">User Terbaru</h3>
           </div>
-          {/* tabel user */}
-          <section className="mt-4">
-          <div className="relative overflow-x-auto">
-            <table className="text-left text-sm text-gray-500 w-full">
-              <thead className="bg-gray-50 text-xs uppercase text-gray-700">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    No
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Nama
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Email
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Tipe
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {artikel.map((artikel, index) => (
-                  <tr key={artikel._id} className="border-b bg-white">
-                    <td className="px-6 py-4">{index + 1}</td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                        
-                    </td>
-                    <td className="max-w-xs overflow-hidden text-ellipsis whitespace-nowrap px-6 py-4">
-                      {artikel.judul.length > 20
-                        ? `${artikel.name.substring(0, 25)}...`
-                        : artikel.judul}
-                    </td>
-                    <td className="px-6 py-4">
-                      {new Date(artikel.tanggal).toLocaleDateString("id-ID")}
-                    </td>
-                    <td className="w-96 px-6 py-4 ">
-                      <p>{artikel.teks}</p>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          {/* tabel new user */}
+          <section className="mt-4 ">
+            <NewUserTable></NewUserTable>
           </section>
         </section>
-      </section>
+      </main>
     </DashboardLayout>
   );
 };
