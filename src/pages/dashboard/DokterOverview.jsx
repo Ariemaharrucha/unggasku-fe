25426ea3f1f423a6a2d0c02fec2dc65a5bc1c6ab
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-09 10:37:21 +0700

    add dokter overview

--- src/pages/dashboard/DokterOverview.jsx
+++ src/pages/dashboard/DokterOverview.jsx
@@ -1,27 +1,24 @@
+import { FiPlusCircle } from "react-icons/fi";
+import Button from "../../components/ui/Button.jsx";
 import { Link } from "react-router-dom";
 import { DashboardLayout } from "../../layouts/DashboardLayout.jsx";
-import Button from "../../components/ui/Button.jsx";
-import { ArtikelTable } from "./ArtikelTable.jsx";
-import { FiPlusCircle } from "react-icons/fi";
 
-export const Artikel = () => {
+export const DokterOverview = () => {
   return (
     <DashboardLayout>
       <main className="max-w-6xl">
         <section className="sticky top-0 z-[2] flex items-center justify-between bg-white py-2 ">
           <div>
-            <h3 className="text-xl">Artikel</h3>
+            <h3 className="text-xl">Daftar Dokter</h3>
           </div>
-          <Link to="/dashboard/artikel/create">
+          <Link to="/dashboard/dokter/create">
             <Button variant="secondary">
               <FiPlusCircle size={24} />
-              Buat Artikel
+              Tambah Dokter
             </Button>
           </Link>
         </section>
-        <section className="mt-4">
-          <ArtikelTable></ArtikelTable>
-        </section>
+        <section className="mt-4">{/* list dokter */}</section>
       </main>
     </DashboardLayout>
   );
