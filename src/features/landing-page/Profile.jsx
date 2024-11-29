9fc0ed36be3743b0cdf37d4af2f51436a8f8ee4c
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-29 15:33:27 +0700

    add edit profile

--- src/features/landing-page/Profile.jsx
+++ src/features/landing-page/Profile.jsx
@@ -1,22 +1,91 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-
-import fotoProfil from "../../assets/card profil-tentang-kami1.jpg";
 import { Navbar } from "../../components/shared/Navbar.jsx";
+import { useParams } from "react-router-dom";
+import { getUser } from "./services/api.profile.js";
+import { editProfile } from "./services/api.profile.js";
 
 export const UserProfile = () => {
-  const [fullName, setFullName] = useState("Stefanus Fandi Wibowo");
-  const [email, setEmail] = useState("stefanusfandi45@gmail.com");
-  const [password, setPassword] = useState("password123");
+  const { id } = useParams();
+  const idUser = parseInt(id);
+  const [userData, setUserData] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState(""); 
+  const [passwordError, setPasswordError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("")
+  const fileInputRef = useRef(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const response = await getUser(idUser);
+        setUserData(response[0]);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+    fetchUser();
+  }, [idUser]);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handlePasswordUpdate = () => {
-    alert("Password updated successfully!");
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUserData((prev) => ({...prev, [name]: value}))
+    console.log(value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8) {
+      setPasswordError("Password must be at least 8 characters.");
+    } else {
+      setPasswordError("");
+    }
   };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    if (fileInputRef.current.files[0]) {
+      console.log(fileInputRef.current.files[0]);
+      
+        formData.append("image_profile", fileInputRef.current.files[0]);
+    } else {
+        formData.append("image_profile", userData.image_profile);
+    }
+
+    formData.append("username", userData.username);
+    formData.append("email", userData.email);
+
+    if (password) {
+        formData.append("password", password);
+    }
+
+    try {
+        const response = await editProfile(idUser, formData);
+        alert("Profil berhasil diperbarui!");
+        setUserData(response);
+        setSuccessMessage("update succes")
+    } catch (error) {
+        console.error("Error updating profile:", error);
+        console.log(fileInputRef.current.files[0]);
+        
+        alert("Gagal memperbarui profil.");
+    }
+};
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
@@ -25,53 +94,72 @@ export const UserProfile = () => {
         <h1 className="text-3xl text-center font-bold text-gray-800 mb-8">
           Selamat Datang di Halaman Akun Anda
         </h1>
-
+        {/* image */}
         <div className="flex flex-col items-center mb-8">
           <img
-            src={fotoProfil}
+            src={
+              userData?.image_profile ||
+              "https://i.pinimg.com/736x/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg"
+            }
             alt="Profile"
             className="w-32 h-32 rounded-full mb-4 border-2 border-gray-300"
           />
-          <button className="text-primary-500 hover:underline focus:outline-none">
+          <button
+            className="text-primary-500 hover:underline focus:outline-none"
+            onClick={handleFileInputClick}
+          >
             Ganti Gambar Profil
           </button>
+          <input
+            type="file"
+            className="w-full p-3 mt-2 border rounded-md hidden"
+            name="image_profile"
+            ref={fileInputRef}
+            accept=".jpeg,.jpg,.png"
+            onChange={(e) => handleChange(e)}
+          />
         </div>
-
+        {/* usernname */}
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-medium mb-1">
             Nama Lengkap
           </label>
           <input
             type="text"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            disabled
           />
         </div>
-
+        {/* email */}
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-medium mb-1">
             Email
           </label>
           <input
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            disabled
           />
         </div>
-
+        {/* password */}
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-medium mb-1">
             Kata Sandi
           </label>
-          <div className="flex items-center">
+          <p className="text-sm">
+            <span className="text-red-500 font-bold">Note: </span>
+            <span>Password yang lama akan diganti</span>
+          </p>
+          <div className="flex items-center mt-2">
             <input
               type={showPassword ? "text" : "password"}
+              name="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <button
@@ -81,18 +169,23 @@ export const UserProfile = () => {
               {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
             </button>
           </div>
+          {passwordError && (
+            <p className="text-red-500 text-sm mt-2">{passwordError}</p>
+          )}
         </div>
-
+          
         <div>
           <button
-            onClick={handlePasswordUpdate}
+          onClick={handleSubmit}
             className="px-4 py-2 bg-primary-500 text-white font-semibold rounded-md hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50"
           >
-            Perbarui Kata Sandi
+            Simpan Perubahan
           </button>
         </div>
+        {successMessage && (
+          <p className="text-green-500 text-sm mt-2">{successMessage}</p>
+        )}
       </div>
     </div>
   );
 };
-
