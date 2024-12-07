7cba02a6f50b36405939a23b084b10a7ce35f8ce
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-12-07 21:08:33 +0700

    add fetchdokter & total user, artikel, dokter

--- src/features/dashboard-admin/components/index.dashboard.admin.jsx
+++ src/features/dashboard-admin/components/index.dashboard.admin.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { LuFiles } from "react-icons/lu";
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
 import { NewUserTable } from "./NewUserTable.jsx";
 import { DashboardAdminLayout } from "../../../layouts/DashboardAdminLayout.jsx";
+import axios from "axios";
 
 export const DashboardAdmin = () => {
+  const [totalArtikel, setTotalArtikel] = useState(0);
+  const [totalDokter, setTotalDokter] = useState(0);
+  const [totalUser, setTotalUser] = useState(0);
+  const [newUsers, setNewUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        setLoading(true);
+        const [
+          artikelResponse,
+          dokterResponse,
+          userResponse,
+          newUsersResponse,
+        ] = await Promise.all([
+          axios.get(`${import.meta.env.VITE_API_URL}/admin/artikel/total`),
+          axios.get(`${import.meta.env.VITE_API_URL}/admin/dokter/total`),
+          axios.get(`${import.meta.env.VITE_API_URL}/admin/user/total`),
+          axios.get(`${import.meta.env.VITE_API_URL}/admin/user?limit=10`),
+        ]);
+
+        setTotalArtikel(artikelResponse.data.data[0].total_artikel);
+        setTotalDokter(dokterResponse.data.data[0].total);
+        setTotalUser(userResponse.data.data[0].total);
+        setNewUsers(newUsersResponse.data.data);
+      } catch (error) {
+        console.error("Failed to fetch dashboard data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDashboardData();
+  }, []);
+
   return (
     <DashboardAdminLayout>
       <main className="max-w-6xl w-full">
@@ -21,9 +58,17 @@ export const DashboardAdmin = () => {
 
         <section className="mt-6">
           <div className="grid grid-cols-12 gap-6">
-            <StatisticCard title="Artikel" count={100} icon={<LuFiles />} />
-            <StatisticCard title="Dokter" count={100} icon={<FaUserDoctor />} />
-            <StatisticCard title="User" count={100} icon={<FaUser />} />
+            <StatisticCard
+              title="Artikel"
+              count={totalArtikel}
+              icon={<LuFiles />}
+            />
+            <StatisticCard
+              title="Dokter"
+              count={totalDokter}
+              icon={<FaUserDoctor />}
+            />
+            <StatisticCard title="User" count={totalUser} icon={<FaUser />} />
           </div>
         </section>
 
@@ -36,7 +81,17 @@ export const DashboardAdmin = () => {
           </div>
           {/* tabel new user */}
           <section className="mt-4 ">
-            <NewUserTable></NewUserTable>
+            {loading ? (
+              <div className="flex justify-center items-center py-4">
+                <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500"></div>
+              </div>
+            ) : newUsers.length > 0 ? (
+              <NewUserTable data={newUsers} />
+            ) : (
+              <p className="text-center py-4 text-gray-500">
+                No data available.
+              </p>
+            )}
           </section>
         </section>
       </main>
@@ -44,13 +99,18 @@ export const DashboardAdmin = () => {
   );
 };
 
-const StatisticCard = ({ title, count, icon, iconSize = 70 }) => {
+// eslint-disable-next-line react/prop-types
+const StatisticCard = ({ title, count, icon, iconSize = 70, loading }) => {
   const iconWithSize = React.cloneElement(icon, { size: iconSize });
   return (
     <div className="col-span-4 py-4 px-10 bg-secondary-300 rounded-2xl flex items-center justify-around h-32">
       <div className="w-2/3">
         <p className="text-lg font-semibold text-slate-600">Total {title}</p>
-        <p className="text-2xl py-2 font-semibold">{count}</p>
+        {loading ? (
+          <div className="animate-pulse h-6 bg-gray-300 rounded-md w-20 my-2"></div>
+        ) : (
+          <p className="text-2xl py-2 font-semibold">{count}</p>
+        )}
       </div>
       <div className="w-auto flex justify-center">{iconWithSize}</div>
     </div>
